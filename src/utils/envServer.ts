42b943a21e404a60f928b86266ac799374484d86
SaidBC <razzouksaid139@gmail.com>
2025-03-14 03:21:58 +0000

    revert: Update DATABASE_URL handling in envServer.ts

--- src/utils/envServer.ts
+++ src/utils/envServer.ts
@@ -1,15 +1,19 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  DATABASE_URL: z
-    .string()
-    .refine(
-      (val) => val.startsWith("postgresql://") || val.startsWith("postgres://"),
-      {
-        message: 'String must start with "postgresql://" or "postgres://"',
-      }
-    ),
+  DATABASE_URL: z.string().refine(
+    (val) => {
+      console.log(val);
+      return val.startsWith("postgresql://") || val.startsWith("postgres://");
+    },
+    {
+      message: 'String must start with "postgresql://" or "postgres://"',
+    }
+  ),
   REWARDS_HUB_ID: z.string(),
 });
 
-export default envSchema.parse(process.env);
+export default envSchema.parse({
+  DATABASE_URL: process.env.DATABASE_URL,
+  REWARDS_HUB_ID: process.env.DATABASE_URL,
+});
