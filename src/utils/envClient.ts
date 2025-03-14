ac47baba02d8022fa81158fd7d2d6d8892119fc0
SaidBC <razzouksaid139@gmail.com>
2025-03-14 03:46:39 +0000

    chore: remove logs

--- src/utils/envClient.ts
+++ src/utils/envClient.ts
@@ -1,8 +1,5 @@
 import { z } from "zod";
-console.log(
-  process.env.NEXT_PUBLIC_API_URL,
-  process.env.NEXT_PUBLIC_BINANCE_URL
-);
+
 const envSchema = z.object({
   NEXT_PUBLIC_API_URL: z.string().url(),
   NEXT_PUBLIC_BINANCE_URL: z.string().url(),
